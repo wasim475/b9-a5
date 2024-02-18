c3b551df714c2da58b944929346f91ba7d29c954
wasim475 <wasimhossain575@gmail.com>
2024-02-18 16:07:24 +0600

    buy now button's js task done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('connected')
+
+/*=============================================================
+        #Buy ticket part start
+==============================================================*/
+
+document.getElementById('buyTicketBtn').addEventListener('click',function(){
+    let phParibahan = document.getElementById('phParibahan')
+    
+    phParibahan.scrollIntoView({ behavior: 'smooth' })
+    
+})
+
+/*=============================================================
+        #Buy ticket part End
+==============================================================*/
